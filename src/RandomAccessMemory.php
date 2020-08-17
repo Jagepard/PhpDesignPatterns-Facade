082a1bca5618b44236184b0dd333613f11e63b2c
jagepard <dankorot@gmail.com>
2020-08-17 05:13:10 -0700

    Update

--- src/RandomAccessMemory.php
+++ src/RandomAccessMemory.php
@@ -3,9 +3,10 @@
 declare(strict_types=1);
 
 /**
- * @author    : Korotkov Danila <dankorot@gmail.com>
- * @license   https://mit-license.org/ MIT
+ * @author  : Jagepard <jagepard@yandex.ru>
+ * @license https://mit-license.org/ MIT
  */
+
 namespace Structural\Facade;
 
 class RandomAccessMemory
